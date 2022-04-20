0d930dcbb78d16d492b57aa3bb4281b7342ec763
Enhance <103292211+Enhaance@users.noreply.github.com>
2022-04-20 17:34:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function doUpdateParagraph() {
+  var inputElement = document.getElementById('inputText');
+  var outputElement = document.getElementById('outputParagraph');
+  var message = inputElement.value;
+  outputElement.textContent = message;
+}
+
+// Made by @enhaance on github
